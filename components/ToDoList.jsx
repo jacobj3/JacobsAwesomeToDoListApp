fa97fd95a17c244d4479068db68c4ca772f96241
jacobj3 <131091976+jacobj3@users.noreply.github.com>
2024-02-28 16:11:58 -0700

    Lab 3

--- components/ToDoList.jsx
+++ components/ToDoList.jsx
@@ -8,27 +8,17 @@ import {
   Text,
 } from 'react-native';
 
-function ToDoList(props) {
+function ToDoList({tasks}) {
   return (
-    <>
-      <ScrollView>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Do laundry</Text>
+    <ScrollView>
+      {tasks.map((task, index) => (
+        <Pressable key={index}>
+          <View style={styles.task}>
+            <Text style={styles.taskText}>{task}</Text>
           </View>
         </Pressable>
-        <Pressable>
-          <View style={[styles.task]}>
-            <Text style={styles.taskText}>Go to gym</Text>
-          </View>
-        </Pressable>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Walk dog</Text>
-          </View>
-        </Pressable>
-      </ScrollView>
-    </>
+      ))}
+    </ScrollView>
   );
 }
 
